2f57ffe827525412c10811a3a5998aa8b09fef04
Schalk Van Der Merwe <schalk83.vandermerwe@gmail.com>
2025-02-19 15:13:54 +0200

    Feb 2025 18t

--- FreschOne/obj/Debug/net8.0/FreschOne.AssemblyInfo.cs
+++ FreschOne/obj/Debug/net8.0/FreschOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreschOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b46b068b94a6c30ddeaff083d5b6c5c3991cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae27968c24449c85bbaa08a84943dc4c11740848")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreschOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreschOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
